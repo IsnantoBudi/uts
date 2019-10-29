bd928dc3a378f7acc51a198a24814fabb859f1b4
IsnantoBudi <45707679+IsnantoBudi@users.noreply.github.com>
2019-10-29 15:41:39 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/gmail/isnantobudi0/uts/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.gmail.isnantobudi0.uts
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
